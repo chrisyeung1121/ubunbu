7e236a6d766ca7dce23ec53858af43db6e051766
Christopher Yeung <chrisyeung@mac.com>
2021-12-22 16:53:22 +0000

    added frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Christopher Yeung - Entrepreneur, Management Consultant, Technologist and Product </title>
+        <meta name="description" content="Christopher Yeung currently works at EY Parthenon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Christopher is awesome.
+        </h1>
+
+        <p className={styles.description}>
+          Contact me if you want to understand the following topics
+        </p>
+
+        <div className={styles.grid}>
+          <a className={styles.card}>M&A</a>
+          <a className={styles.card}>Serverless</a>
+          <a className={styles.card}>Venture Building</a>
+          <a className={styles.card}>Car Ecosystem</a>
+          <a className={styles.card}>Design - Figma</a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Contact me at hi@chrisyeung.me</p>
+      </footer>
+    </div>
+  )
+}
