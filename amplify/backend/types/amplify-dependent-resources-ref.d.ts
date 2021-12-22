1d1ca74d847634802024579b3c485bfc965d8a60
Christopher Yeung <chrisyeung@mac.com>
2021-12-22 18:18:20 +0000

    update api with fargate container

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "container3af8c064": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "ApiName": "string",
+            "RootUrl": "string"
+        }
+    }
+}
