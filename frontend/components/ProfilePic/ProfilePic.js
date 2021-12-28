58f99134d93d6de91b17c835eed46f6a0ae8a853
Christopher Yeung <chrisyeung@mac.com>
2021-12-28 13:22:03 +0000

    update profile pic

--- frontend/components/ProfilePic/ProfilePic.js
+++ frontend/components/ProfilePic/ProfilePic.js
@@ -1,10 +1,17 @@
 import Image from 'next/image'
 import myPic from './me.jpg'
 
+
 function ProfilePic() {
   return (
     <div>
-      <Image src={myPic} alt="my profile pic" />
+      <Image
+        src={myPic}
+        alt="my profile pic"
+        width={500}
+        height={500}
+        priority
+      />
     </div>
   );
 }
